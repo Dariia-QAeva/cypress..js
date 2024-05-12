ac6f6b733dfa1802a4bce5c2cd534844e17f5307
Dariia QAeva <162054833+Dariia-QAeva@users.noreply.github.com>
2024-05-12 22:36:06 +0300

    Add files via upload

---
+++ cypress/e2e/pokemons.cy.js
@@ -0,0 +1,17 @@
+describe('Покупка аватара', function () {                               
+    it('e2e тест на покупку нового аватара для тренера', function () {  
+         cy.visit('https://pokemonbattle.me/');                          
+         cy.get('input[type="email"]').type('LOGIN');      
+         cy.get('input[type="password"]').type('PASSWORD');    
+         cy.get('button[type="submit"]').click();                        
+         cy.get('.header__btns > [href="/shop"]').click();               
+         cy.get('.available > button').first().click();                  
+         cy.get('.credit').type('4620869113632996');                     
+         cy.get('.k_input_date').type('1225');                           
+         cy.get('.k_input_name').type('NAME');                           
+         cy.get('.pay-btn').click();                                   
+         cy.get('#cardnumber').type('56456');                            
+         cy.get('.payment__submit-button').click();                      
+         cy.contains('Покупка прошла успешно').should('be.visible');    
+     });
+ });
